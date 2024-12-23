333c47563d59553e8b81313a1c0996963c8211ad
AronJustoC <aronchoque727@gmail.com>
2024-12-22 20:06:22 -0500

    implementacion de impresion de rastro del jugador

--- laberinto.cpp
+++ laberinto.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream> //Libreria para entrada y salida
 #include <ncurses.h>
 #include <random>
@@ -18,7 +20,7 @@ private:
 
 public:
   // keyword "explicit": hace que el tipo de argumento sea especificado
-  explicit Laberinto(const size_type x, const size_type y) : ancho{x}, alto{y} {
+  Laberinto(const size_type x, const size_type y) : ancho{x}, alto{y} {
     // Asignacion y inicializacion de la matriz
     laberinto = new char[ancho * alto];
     for (size_type i = 0; i < alto * ancho; i++) {
@@ -93,6 +95,33 @@ public:
     }
   }
 
+  void imprimir_rastro(const Color &color1, const Color &color2,
+                       const Color &color3) {
+    move(0, 0);
+    for (size_type y = 0; y < alto; ++y) {
+      for (size_type x = 0; x < ancho; ++x) {
+        int celda = laberinto[y * ancho + x];
+        if (celda == 1) {
+          attron(COLOR_PAIR(color1));
+          printw("[]"); // Pared
+          attroff(COLOR_PAIR(color1));
+        } else if (celda == 2) {
+          attron(COLOR_PAIR(color2));
+          printw(". "); // Pasó dos veces
+          attroff(COLOR_PAIR(color2));
+        } else if (celda == 3) {
+          attron(COLOR_PAIR(color3));
+          printw("x "); // Pasó tres veces
+          attroff(COLOR_PAIR(color3));
+        } else {
+          printw("  "); // Camino vacío
+        }
+      }
+      printw("\n");
+    }
+    refresh();
+  }
+
   void imprimir(const Color &color) {
     attron(COLOR_PAIR(color));
     move(0, 0);
@@ -105,7 +134,7 @@ public:
     attroff(COLOR_PAIR(color));
   }
 
-  const char *getlaberinto() const noexcept { return laberinto; }
+  char *getlaberinto() noexcept { return laberinto; }
   const size_type getancho() const noexcept { return ancho; }
   const size_type getalto() const noexcept { return alto; }
 };
@@ -119,8 +148,7 @@ private:
   size_type contador;
 
 public:
-  explicit Jugador(const size_type xx, const size_type yy)
-      : x{xx}, y{yy}, contador{0} {}
+  Jugador(const size_type xx, const size_type yy) : x{xx}, y{yy}, contador{0} {}
 
   Jugador() : x{1}, y{0}, contador{0} {}
 
@@ -133,37 +161,109 @@ public:
 
   void imprimirInfo(const Laberinto &m, const Color &color) {
     attron(COLOR_PAIR(color));
-    move(m.getalto() + 1, 0); // Impresion una linea debajo del laberinto
-    printw("Posicion actual X: %li, Y: %li\n Total de movimientos: %li", x, y,
-           contador);
+    move(m.getalto() + 1, 0); // Impresión una línea debajo del laberinto
+    printw("========================================\n");
+    printw("Posicion actual: X = %li, Y = %li\n", x, y);
+    printw("Total de movimientos: %li\n", contador);
+    printw("========================================\n");
     attroff(COLOR_PAIR(color));
+    refresh(); // Actualizar la pantalla
   }
 
-  void imprimirGanador(const Color &color) {
-    move(0, 0);
-    clear();
-    refresh();
-    attron(COLOR_PAIR(color));
-    printw("Ganaste!!!\n Total de movimientos %li", contador);
-    getch();
-    attroff(COLOR_PAIR(color));
-  }
+  void mov(Laberinto &m, const size_type xx, const size_type yy) {
+    auto mz = m.getlaberinto(); // Obtiene el laberinto para mover el personaje
 
-  void mov(const Laberinto &m, const size_type xx, const size_type yy) {
-    static auto mz =
-        m.getlaberinto(); // obtiene el laberinto para poder mover el personaje
-
-    // Si pa posicion estan fuera del laberinto no se mueve
+    // Si la posición está fuera del laberinto, no se mueve
     if (x + xx <= 0 || x + xx >= m.getancho() || y + yy <= 0 ||
         y + yy >= m.getalto())
       return;
-    // Si el laberinto esta ocupado por un muro no se mueve
+
+    // Si el laberinto está ocupado por un muro, no se mueve
     if (mz[(y + yy) * m.getancho() + (x + xx)] == 1)
       return;
 
+    if (mz[y * m.getancho() + x] == 0) {
+      mz[y * m.getancho() + x] = 2; // Primera vez
+    } else if (mz[y * m.getancho() + x] == 2) {
+      mz[y * m.getancho() + x] = 3; // Segunda vez
+    }
+
+    // Mueve el personaje
     x += xx;
     y += yy;
+
     ++contador;
+  }
+
+  void solverManoIzquierda(Laberinto &m) {}
+
+  void guardarPuntuacion() {
+    ofstream file;
+
+    // Apertura en modo de adición
+    file.open("puntuacion.txt", ios::app);
+
+    if (!file) {
+      cerr << "Error: No se pudo abrir el archivo 'puntuacion.txt'." << endl;
+      return;
+    }
+
+    int puntuacionBase = 1000;
+    int puntuacion = puntuacionBase - contador;
+
+    // Guardar la puntuación en el archivo
+    file << puntuacion << endl;
+
+    file.close();
+
+    cout << "\nPuntuación guardada exitosamente: " << puntuacion << endl;
+    cout << "========================================\n";
+  }
+
+  vector<int> listarTopCincoPuntuaciones() {
+    ifstream file("puntuacion.txt");
+    if (!file) {
+      cerr << "Error: No se pudo abrir el archivo 'puntuacion.txt'." << endl;
+      return {};
+    }
+
+    vector<int> puntuaciones;
+    int puntuacion;
+
+    // Leer las puntuaciones desde el archivo
+    while (file >> puntuacion) {
+      puntuaciones.push_back(puntuacion);
+    }
+    file.close();
+
+    // Ordenar en orden descendente
+    sort(puntuaciones.begin(), puntuaciones.end(), greater<int>());
+
+    return puntuaciones;
+  }
+
+  void imprimirGanador(const Color &color) {
+    clear();
+    refresh();
+    attron(COLOR_PAIR(color));
+    printw("\n========================================\n");
+    printw("                ¡Ganaste!                \n");
+    printw("========================================\n");
+    printw("Puntuación: %li\n", 1000 - contador);
+    printw("Total de movimientos: %li\n", contador);
+    printw("========================================\n");
+
+    guardarPuntuacion();
+    printw("\nPresiona cualquier tecla para continuar...");
+    printw("Los 5 mejores puntuaciones:\n");
+    printw("========================================\n");
+    for (size_t i = 0; i < min(listarTopCincoPuntuaciones().size(), size_t(5));
+         i++) {
+      cout << i + 1 << ". " << listarTopCincoPuntuaciones()[i] << endl;
+    }
+    cout << "========================================\n";
+    getch();
+    attroff(COLOR_PAIR(color));
   }
 
   const size_type getpos_x() const noexcept { return x; }
@@ -204,6 +304,22 @@ public:
 };
 
 int main() {
+  // Inicializar ncurses
+  initscr();            // No mostrar lo que escribe el usuario
+  cbreak();             // Desactivar buffering de línea
+  keypad(stdscr, TRUE); // Activar las teclas especiales como las de dirección
+
+  // Mostrar opciones en ncurses
+  printw("Introduzca la opción deseada:\n");
+  printw("[1] Jugar\n");
+  printw("[2] Apoyando la mano izquierda a la pared (DFS)\n");
+  printw("[3] Recorrer el laberinto y probar cada callejón usando solo "
+         "(Backtracking)\n");
+
+  int opcion;
+  scanw("%d", &opcion); // Obtener la opción del usuario en ncurses
+
+  // Obtener la ventana de la terminal
   VentanaCurses miVentana;
   int ancho = 23, alto = 15;
   while (ancho < (miVentana.getx() / 2) - 3)
@@ -211,48 +327,77 @@ int main() {
   while (alto < miVentana.gety() - 4)
     alto += 2;
 
-  bool banderaSalir = false;
-
-  // Crear un laberinto y un jugador
   Laberinto m(ancho, alto);
   Jugador p;
-  int ch = 0;
+  bool banderaSalir = false;
 
-  while (!banderaSalir) {
-    m.imprimir(Color::verde);
+  // Seleccionar la opción
+  switch (opcion) {
+  case 1: {
+    // Crear un laberinto y un jugador
+    int ch = 0;
+    while (!banderaSalir) {
+      m.imprimir_rastro(Color::verde, Color::blanco,
+                        Color::rojo);   // Mostrar el laberinto
+      p.imprimir(Color::rojo);          // Mostrar  el jugador
+      p.imprimirInfo(m, Color::blanco); // Mostrar la posición del jugador
+      ch = getch();
+
+      switch (ch) {
+      case 'w':
+      case KEY_UP:
+        p.mov(m, 0, -1);
+        break;
+      case 's':
+      case KEY_DOWN:
+        p.mov(m, 0, 1);
+        break;
+      case 'a':
+      case KEY_LEFT:
+        p.mov(m, -1, 0);
+        break;
+      case 'd':
+      case KEY_RIGHT:
+        p.mov(m, 1, 0);
+        break;
+      case 'q':
+      case 27:
+        banderaSalir = true;
+        break;
+      default:
+        refresh();
+      }
+
+      // Verificar si se ha llegado al final
+      if (p.getpos_y() == m.getalto() - 1) {
+        p.imprimirGanador(Color::blanco);
+        banderaSalir = true;
+      }
+    }
+    break;
+  }
+  case 2:
+    m.imprimir_rastro(Color::verde, Color::blanco, Color::rojo);
     p.imprimir(Color::rojo);
     p.imprimirInfo(m, Color::blanco);
-    ch = getch();
-
-    switch (ch) {
-    case 'w':
-    case KEY_UP:
-      p.mov(m, 0, -1);
-      break;
-    case 's':
-    case KEY_DOWN:
-      p.mov(m, 0, 1);
-      break;
-    case 'a':
-    case KEY_LEFT:
-      p.mov(m, -1, 0);
-      break;
-    case 'd':
-    case KEY_RIGHT:
-      p.mov(m, 1, 0);
-      break;
-    case 'q':
-    case KEY_EXIT:
-    case 27:
-      banderaSalir = true;
-      break;
-    default:
-      refresh();
-    }
-    // verificar si se ha llegado al final
-    if (p.getpos_y() == m.getalto() - 1) {
-      p.imprimirGanador(Color::blanco);
-      banderaSalir = true;
-    }
+    // p.solverManoIzquierda(m);
+    break;
+  case 3:
+    m.imprimir_rastro(Color::verde, Color::blanco, Color::rojo);
+    p.imprimir(Color::rojo);
+    p.imprimirInfo(m, Color::blanco);
+    // p.solverBacktracking(m);
+    break;
+  default:
+    printw("Opción no válida.\n");
+    break;
   }
+
+  // Esperar a que el usuario presione una tecla
+  printw("\nPresione cualquier tecla para continuar...");
+  getch();
+
+  // Finalizar ncurses
+  endwin();
+  return 0;
 }
